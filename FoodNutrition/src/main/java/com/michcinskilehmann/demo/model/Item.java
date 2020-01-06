8d81ab7aae614b0fa4079c9cefc342a2f4fb2159
Paweł <48561670+PawelM98@users.noreply.github.com>
2020-01-06 16:04:41 +0100

    Add files via upload

---
+++ FoodNutrition/src/main/java/com/michcinskilehmann/demo/model/Item.java
@@ -0,0 +1,109 @@
+
+package com.michcinskilehmann.demo.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "day",
+    "mealPlanId",
+    "slot",
+    "position",
+    "type",
+    "value"
+})
+public class Item {
+
+    @JsonProperty("day")
+    private Integer day;
+    @JsonProperty("mealPlanId")
+    private Integer mealPlanId;
+    @JsonProperty("slot")
+    private Integer slot;
+    @JsonProperty("position")
+    private Integer position;
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("value")
+    private String value;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("day")
+    public Integer getDay() {
+        return day;
+    }
+
+    @JsonProperty("day")
+    public void setDay(Integer day) {
+        this.day = day;
+    }
+
+    @JsonProperty("mealPlanId")
+    public Integer getMealPlanId() {
+        return mealPlanId;
+    }
+
+    @JsonProperty("mealPlanId")
+    public void setMealPlanId(Integer mealPlanId) {
+        this.mealPlanId = mealPlanId;
+    }
+
+    @JsonProperty("slot")
+    public Integer getSlot() {
+        return slot;
+    }
+
+    @JsonProperty("slot")
+    public void setSlot(Integer slot) {
+        this.slot = slot;
+    }
+
+    @JsonProperty("position")
+    public Integer getPosition() {
+        return position;
+    }
+
+    @JsonProperty("position")
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("type")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @JsonProperty("value")
+    public String getValue() {
+        return value;
+    }
+
+    @JsonProperty("value")
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
